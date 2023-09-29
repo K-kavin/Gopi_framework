4d14c72a9b5f8eae5269b971338b7e4ee2408c73
admin <admin@DESKTOP-A7U52L2>
2023-09-29 20:58:12 +0530

    Build B01

---
+++ Kelvin_New_JAva2/src/practice/First_program.java
@@ -0,0 +1,19 @@
+package practice;
+
+import java.util.ArrayList;
+
+public class First_program
+{
+	public static void main(String[] args) 
+	{
+		ArrayList<String> l=new ArrayList<String>();
+		l.add("A");
+		l.add("B");
+		l.add("C");
+		l.add("D");
+		l.add("E");
+		System.out.println(l);
+		l.add(1,"F");
+		System.out.println(l);
+	}
+}
